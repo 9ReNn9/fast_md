1526305a3a56bc2cd9c5beac3acd371655d5f525
Renat Levcenco <renatlevcenco@gmail.com>
2022-04-03 20:34:25 +0300

    added links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def hosting
+  end
+
+  def vpsvds
+  end
+
+  def dedicated
+  end
+  def domens
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  #get 'home/index'
+  get 'home/hosting'
+  get 'home/vpsvds'
+  get 'home/dedicated'
+  get 'home/domens'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
